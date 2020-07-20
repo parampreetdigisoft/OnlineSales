34e9cef6740a76911c868deea60964de3cc008bf
Developer <Developer>
2020-07-20 20:36:00 +0530

    commit for stripe changes

---
+++ OnlineSales.Web/ViewModel/StripeResponseViewModel.vb
@@ -0,0 +1,9 @@
+﻿Public Class StripeResponseViewModel
+    Public Property access_token As String
+    Public Property scope As String
+    Public Property livemode As String
+    Public Property token_type As String
+    Public Property refresh_token As String
+    Public Property stripe_user_id As String
+    Public Property stripe_publishable_key As String
+End Class
