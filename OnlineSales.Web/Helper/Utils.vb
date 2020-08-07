c7499a1306ee046ffecfd16d3c78658eaae8cd11
Developer <Developer>
2020-08-07 13:39:22 +0530

    Error handling for email process

--- OnlineSales.Web/Helper/Utils.vb
+++ OnlineSales.Web/Helper/Utils.vb
@@ -9,7 +9,6 @@ Public Class Utils
     Public Shared Property Account As String
     Public Shared Property Port As String
     Public Shared Property Host As String
-    Public Shared Property StripeApiKey As String
 
     ''' <summary>
     ''' send mail
@@ -18,34 +17,40 @@ Public Class Utils
     ''' <param name="body"></param>
     ''' <param name="subject"></param>
     ''' <param name="cc"></param>
-    Public Shared Sub SendEmail(ByVal emailTo As String, ByVal body As String, ByVal subject As String, ByVal Optional cc As String = Nothing)
-        Using client As SmtpClient = New SmtpClient()
-            Dim credential As NetworkCredential = New NetworkCredential With {
-                .UserName = Account,
-                .Password = SystemPassword
-            }
-            client.UseDefaultCredentials = False
-            client.Credentials = credential
-            client.Host = Host
-            client.Port = Convert.ToInt32(Port)
-            client.EnableSsl = True
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+    Public Shared Function SendEmail(ByVal emailTo As String, ByVal body As String, ByVal subject As String, ByVal Optional cc As String = Nothing) As Boolean
+        Dim isSent = False
+        Try
+            Using client As SmtpClient = New SmtpClient()
+                Dim credential As NetworkCredential = New NetworkCredential With {
+                    .UserName = Account,
+                    .Password = SystemPassword
+                }
+                client.UseDefaultCredentials = False
+                client.Credentials = credential
+                client.Host = Host
+                client.Port = Convert.ToInt32(Port)
+                client.EnableSsl = True
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
 
-            Using emailMessage As MailMessage = New MailMessage()
-                emailMessage.[To].Add(New MailAddress(emailTo))
+                Using emailMessage As MailMessage = New MailMessage()
+                    emailMessage.[To].Add(New MailAddress(emailTo))
 
-                If Not String.IsNullOrEmpty(cc) Then
-                    emailMessage.CC.Add(New MailAddress(cc))
-                End If
+                    If Not String.IsNullOrEmpty(cc) Then
+                        emailMessage.CC.Add(New MailAddress(cc))
+                    End If
 
-                emailMessage.From = New MailAddress(SystemEmail)
-                emailMessage.Subject = subject
-                emailMessage.Body = body.ToString()
-                emailMessage.IsBodyHtml = True
-                client.Send(emailMessage)
+                    emailMessage.From = New MailAddress(SystemEmail)
+                    emailMessage.Subject = subject
+                    emailMessage.Body = body.ToString()
+                    emailMessage.IsBodyHtml = True
+                    client.Send(emailMessage)
+                    isSent = True
+                End Using
             End Using
-        End Using
-    End Sub
+        Catch __unusedException1__ As Exception
+            Return isSent
+        End Try
+    End Function
 
     ''' <summary>
     ''' convert store name to subdomain
